32e08cd4e419c5fac5f5d47a8047ab9a197e1f11
Will Albeck <will@albeck.me>
2025-07-11 15:18:27 -0400

    Complete inventory management lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,37 @@
 // Write your code here
+// Declare the products array
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+// Log the first product
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+// Add a new product to the array
+function addProduct(productName) {
+  products.push(productName);
+}
+
+// Update the name of a product at a given index
+function updateProductName(index, newName) {
+  if (index >= 0 && index < products.length) {
+    products[index] = newName;
+  }
+}
+
+// Remove the last product from the array
+function removeLastProduct() {
+  products.pop();
+}
+
+// Export all functions and the array for testing
+module.exports = {
+  products,
+  logFirstProduct,
+  addProduct,
+  updateProductName,
+  removeLastProduct,
+};
 
 
 // Export the necessary parts for testing
